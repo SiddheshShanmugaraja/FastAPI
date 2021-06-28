aff8610b2c83a788f994ceedf6b77065269062c0
Siddhesh S <49221151+SiddheshShan@users.noreply.github.com>
2021-06-28 20:07:11 +0530

    add get method to get blog from database

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.param_functions import Body
-from sqlalchemy.orm.session import Session
+from sqlalchemy.orm import Session
 from .database import engine, SessionLocal
 from . import schemas, models
 
@@ -16,10 +16,16 @@ def get_db():
         db.close()
 
 
-@app.get('/blog')
+@app.post('/blog')
 def create(request : schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
