503056a651be26df04db9e88bef9d2f061957d23
Siddhesh S <49221151+SiddheshShan@users.noreply.github.com>
2021-06-28 23:04:14 +0530

    update and delete module added in main.py

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from fastapi.param_functions import Body
 from sqlalchemy.orm import Session
+from starlette.responses import Response
 from .database import engine, SessionLocal
 from . import schemas, models
 
@@ -16,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)
 def create(request : schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -25,7 +26,37 @@ def create(request : schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the {id} is not available")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request : schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the {id} is not available")
+    blog.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+def show(id,response:Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the {id} is not available")
+        #response.status_code = status.HTTP_404_NOT_FOUND
+        #return {'detail':f"Blog with the {id} is not available"}
+    return blog
