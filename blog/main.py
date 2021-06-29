7d2d173f32a1bf9c9c3d207720109bc1ce792abe
Siddhesh S <49221151+SiddheshShan@users.noreply.github.com>
2021-06-29 16:13:42 +0530

    added tags and relationship

--- blog/main.py
+++ blog/main.py
@@ -20,16 +20,16 @@ def get_db():
         db.close()
 
 
-@app.post('/blog',status_code=status.HTTP_201_CREATED)
+@app.post('/blog',status_code=status.HTTP_201_CREATED,tags=['Blogs'])
 def create(request : schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
-@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT,tags=['Blogs'])
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -39,7 +39,7 @@ def destroy(id, db: Session = Depends(get_db)):
     return 'done'
 
 
-@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED,tags=['Blogs'])
 def update(id, request : schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -49,13 +49,13 @@ def update(id, request : schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog',response_model=List[schemas.ShowBlog])
+@app.get('/blog',response_model=List[schemas.ShowBlog],tags=['Blogs'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog,tags=['Blogs'])
 def show(id,response:Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -65,10 +65,18 @@ def show(id,response:Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post('/user')
+@app.post('/user',response_model=schemas.ShowUser,tags=['Users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}',response_model=schemas.ShowUser,tags=['Users'])
+def get_user(id:int,db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with the {id} is not available")
+    return user
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
@@ -10,6 +10,9 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creator = relationship('User', back_populates="blogs")
 
 
 class User(Base):
@@ -18,5 +21,7 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    email = Column(String)
+    email = Column(String) 
     password = Column(String)
+
+    blogs = relationship("Blog",back_populates="creator")
